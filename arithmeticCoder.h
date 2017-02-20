8b92dc1cf1df6c591b485bce6830f605dcf450f5
melkaylemur <1tata922@mail.ru>
2017-02-21 00:47:34 +0400

    Update arithmeticCoder.h

--- arithmeticCoder.h
+++ arithmeticCoder.h
@@ -10,22 +10,22 @@ class ArithmeticCoder{
 
 private:
 
-int char_to_index[No_of_chars];
-unsigned char index_to_char[No_of_symbols + 1];
-int cum_freq[No_of_symbols + 1];
+	int char_to_index[No_of_chars];
+	unsigned char index_to_char[No_of_symbols + 1];
+	int cum_freq[No_of_symbols + 1];
 
-int high = Top_value;
-int low = 0;
-int bits_to_follow = 0;
-ArithmeticCoderTools *arTools;
+	int high = Top_value;
+	int low = 0;
+	int bits_to_follow = 0;
+	ArithmeticCoderTools *arTools;
 
-code_value value;
-code_value lowDec, highDec;
+	code_value value;
+	code_value lowDec, highDec;
 
-FILE *foutDecode;
-void bit_plus_follow(int bit);
-void start_encoding();
-void start_decoding();
+	FILE *foutDecode;
+	void bit_plus_follow(int bit);
+	void start_encoding();
+	void start_decoding();
 
 public:
 	ArithmeticCoder();
